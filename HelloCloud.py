66b59504d9ac42eae6a3e648f0c8cc84d9d4adcf
ApisitBoy <42163619+ApisitBoy@users.noreply.github.com>
2021-04-07 15:14:01 +0700

    Update HelloCloud.py

--- HelloCloud.py
+++ HelloCloud.py
@@ -3,7 +3,7 @@ server = Flask(__name__)
 
 @server.route("/")
 def hello():
-  return "Hello Apisit form Server"
+  return "Hello อภิสิทธิ์ ดุลยพันธ์ 5906021613093 form Server"
 
 if __name__ == "__main__":
   server.run(host='0.0.0.0' , port=80)
